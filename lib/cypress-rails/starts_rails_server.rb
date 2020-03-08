3d8492053213ef06a381b7944bcc466d368674a4
Justin Searls <searls@gmail.com>
2020-03-08 20:42:39 +0900

    start incorporating capybara's server inline

--- lib/cypress-rails/starts_rails_server.rb
+++ lib/cypress-rails/starts_rails_server.rb
@@ -1,22 +1,43 @@
-require_relative "configures_test_server"
 require_relative "tracks_resets"
+require_relative "server"
 
 module CypressRails
   class StartsRailsServer
-    def initialize
-      @configures_test_server = ConfiguresTestServer.new
-    end
-
     def call(port:, transactional_server:)
-      @configures_test_server.call(port: port)
-      configure_driver!
-      configure_rails_to_run_our_state_reset_on_every_request!(transactional_server)
-      Capybara.app = create_capybara_rack_app
-      start_system_testing_server!
-      Capybara.current_session
-    end
+
+## -- old>
+      # require "capybara"
+      # require "selenium-webdriver"
+      # Capybara.server_port = port || find_available_port
+      # Capybara.always_include_port = true
+      # Capybara.server = :puma, {Silent: false}
+      # Capybara.current_session
+
+      # configure_driver!
+      # configure_rails_to_run_our_state_reset_on_every_request!(transactional_server)
+      # Capybara.app = create_capybara_rack_app
+      # start_system_testing_server!
+      # Capybara.current_session
+    # end
 
     private
+
+    def find_available_port(host)
+      server = TCPServer.new(host, 0)
+      port = server.addr[1]
+      server.close
+
+      # Workaround issue where some platforms (mac, ???) when passed a host
+      # of '0.0.0.0' will return a port that is only available on one of the
+      # ip addresses that resolves to, but the next binding to that port requires
+      # that port to be available on all ips
+      server = TCPServer.new(host, port)
+      port
+    rescue Errno::EADDRINUSE
+      retry
+    ensure
+      server&.close
+    end
 
     def configure_driver!
       # this is only necessary b/c we're piggybacking on the capybara server
