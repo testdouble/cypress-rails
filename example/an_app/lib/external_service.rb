642b3d9ace7ed51c77df20b51969724c295f9b0b
Daniel Flynn <daniel@testdouble.com>
2021-03-31 08:48:48 -0400

    cleanup: remove custom process removal
    Ensuring the  grandchild process of `http-party` closes after run is functionality that doesn't need to exist in this demo application. Keeping  the initializer example clean and
    without adding the complexity  of ensurring processes for all OS are caught and ended do not need to be in this gem.
    
    Closes #70

--- example/an_app/lib/external_service.rb
+++ example/an_app/lib/external_service.rb
@@ -6,28 +6,5 @@ class ExternalService
       }
       Process.detach(@job_pid)
     end
-
-    def stop_service
-      all_processes.each do |pid|
-        Process.kill("SIGINT", pid.to_i)
-      end
-    end
-
-    def all_processes
-      get_child_processes << @job_pid
-    end
-
-    def get_child_processes
-      pipe = IO.popen("ps -ef | grep #{@job_pid}")
-
-      child_pids = pipe.readlines.map { |line|
-        parts = line.split(/\s+/)
-        parts[1] if parts[2] == @job_pid.to_s && parts[1] != pipe.pid.to_s
-      }.compact
-
-      pipe.close
-      # Reverse the child processes to keep the correct tree spawn structure
-      child_pids.reverse
-    end
   end
 end
