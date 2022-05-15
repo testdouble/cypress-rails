d0c37118204acfb2650520e05c88360b183a767d
ashrocket <ashley@raiteri.net>
2022-05-15 00:52:12 -0400

    Gaurd against pre Rails 6 usage

--- lib/cypress-rails/manages_transactions.rb
+++ lib/cypress-rails/manages_transactions.rb
@@ -68,6 +68,7 @@ module CypressRails
     # need to share a connection pool so that the reading connection
     # can see data in the open transaction on the writing connection.
     def setup_shared_connection_pool
+      return unless ActiveRecord::TestFixtures.respond_to?(:setup_shared_connection_pool)
       @legacy_saved_pool_configs ||= Hash.new { |hash, key| hash[key] = {} }
       @saved_pool_configs ||= Hash.new { |hash, key| hash[key] = {} }
 
