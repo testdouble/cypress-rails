dd83d500d882c0da1229d08a0657feec3cedf808
Justin Searls <searls@gmail.com>
2023-06-22 15:26:20 -0400

    hack stuff up to use replay

--- example/cypress.config.js
+++ example/cypress.config.js
@@ -1,13 +1,16 @@
 const { defineConfig } = require('cypress')
+const replay = require('@replayio/cypress')
 
 module.exports = defineConfig({
   // setupNodeEvents can be defined in either
   // the e2e or component configuration
+  browser: 'replay-chromium',
   e2e: {
     setupNodeEvents(on, config) {
-      on('before:browser:launch', (browser = {}, launchOptions) => {
-        /* ... */
-      })
+      replay.default(on, config)
+      return config
+      // on('before:browser:launch', (browser = {}, launchOptions) => {
+      // })
     },
   },
   screenshotsFolder: "tmp/cypress_screenshots",
--- example/cypress/support/e2e.js
+++ example/cypress/support/e2e.js
@@ -14,7 +14,8 @@
 // ***********************************************************
 
 // Import commands.js using ES2015 syntax:
-import './commands';
+import './commands'
+import '@replayio/cypress/support'
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
