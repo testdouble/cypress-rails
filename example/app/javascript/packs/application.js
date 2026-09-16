b3961f8c3a6a85c6a79a547747e4e5a0e6a9d18f
Ross Brandes <ross@testdouble.com>
2026-09-16 14:16:03 -0400

    Refresh JS dependencies
    And update usage of @rails/ujs as a result.
    
    Hopefully this nips a whole lot more Dependabot PRs in the bud.

--- example/app/javascript/packs/application.js
+++ example/app/javascript/packs/application.js
@@ -3,7 +3,7 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-require("@rails/ujs").start()
+require("@rails/ujs").default.start()
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
